80032ed6c8f1b61bb883700df280b4a140718ed8
Adityachess <adityadone9398@gmail.com>
2021-09-27 00:37:23 +0530

    ADD:Welcome message

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakenladder;
+
+public class SnakeAndLadder {
+
+	static int position=0;
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake and Ladder game");
+
+	}
+
+}
